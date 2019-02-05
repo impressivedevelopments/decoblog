86860114675b02af1f2739ec3773e76fac49a70c
Hungry-Silence <__08>
2019-02-05 21:20:39 +0300

    add templates

---
+++ templates/posts-template.php
@@ -0,0 +1,265 @@
+<div class="posts">
+    <div class="row">
+        <div class="col-lg-3">
+            <div class="post-card white">
+                <div class="post-card-body">
+                <div class="post-card-header">
+                    <span class="wrap">
+                        <i class="fas fa-pen-square fa-3x"></i>
+                    </span>
+                </div>
+                    <h6>Travelling</h6>
+                    <h3>Travel besT Adventure</h3>
+                    <h6>January 25, 2016</h6>
+                    <p>Lorem ipsum doloirs sit amet, csatetur
+                        adipisicin, sed does eiusmod tehampor 
+                        incididunt uts laboasre et dolhvaliqua.
+                        Ut enim  vem, nostrasuaations ullamco
+                        labiosi ut saaliquip exoi asea comoodo
+                        Disco choice is the have great work.</p>
+                    <button class="post-card-button">Read More</button>
+                </div>
+                <div class="post-card-footer">
+                    <span>
+                        <i class="far fa-comments"></i>
+                        <p>NO COMMENT</p>
+                    </span>
+                    <div class="social">
+                        <span>
+                            <i class="fab fa-dribbble"></i>
+                            <i class="fab fa-facebook-f"></i>
+                            <i class="fab fa-twitter"></i>
+                            <i class="fab fa-pinterest"></i>
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3">
+            <div class="post-card white">
+            <div class="post-card-image"> 
+                <img class="d-block w-100" src="images/banner.jpg" alt=""> 
+            </div>
+            <div class="post-card-body">
+                <div class="post-card-header">
+                <span class="wrap">
+                    <i class="fas fa-image fa-3x"></i>
+                </span>
+            </div>
+                <h6>Travelling</h6>
+                <h3>Travel besT Adventure</h3>
+                <h6>January 25, 2016</h6>
+                <button class="post-card-button">Read More</button>
+            </div>
+            <div class="post-card-footer">
+                <span>
+                    <i class="far fa-comments"></i>
+                    <p>NO COMMENT</p>
+                </span>
+                <div class="social">
+                    <span>
+                        <i class="fab fa-dribbble"></i>
+                        <i class="fab fa-facebook-f"></i>
+                        <i class="fab fa-twitter"></i>
+                        <i class="fab fa-pinterest"></i>
+                    </span>
+                </div>
+            </div>
+            </div>
+        </div>
+        <div class="col-lg-3">
+            <div class="post-card white">
+            <div class="post-card-body">
+            <div class="post-card-header">
+                <span class="wrap">
+                    <i class="fas fa-pen-square fa-3x"></i>
+                </span>
+            </div>
+                <h6>Travelling</h6>
+                <h3>Travel besT Adventure</h3>
+                <h6>January 25, 2016</h6>
+                <p>Lorem ipsum doloirs sit amet, csatetur <br>
+                    adipisicin, sed does eiusmod tehampor <br>
+                    choice is the have great work.</p>
+                <button class="post-card-button">Read More</button>
+            </div>
+            <div class="post-card-footer">
+                <span>
+                    <i class="far fa-comments"></i>
+                    <p>NO COMMENT</p>
+                </span>
+                <div class="social">
+                    <span>
+                        <i class="fab fa-dribbble"></i>
+                        <i class="fab fa-facebook-f"></i>
+                        <i class="fab fa-twitter"></i>
+                        <i class="fab fa-pinterest"></i>
+                    </span>
+                </div>
+            </div>
+            </div>
+        </div>
+        <div class="col-lg-3">
+            <div class="post-card white">
+            <div class="post-card-body">
+            <div class="post-card-header">
+                <span class="wrap">
+                    <i class="fas fa-pen-square fa-3x"></i>
+                </span>
+            </div>
+                <h6>Travelling</h6>
+                <h3>Travel besT Adventure</h3>
+                <h6>January 25, 2016</h6>
+                <p> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum, eos doloremque magni sequi officiis maiores quis aliquid recusandae nihil nobis, error laboriosam beatae iusto suscipit commodi illum eum, iste esse. Facilis placeat nobis minima pariatur iste minus, expedita corporis cupiditate officiis quam unde sapiente! Consectetur, et id voluptatum provident placeat officia illum quas minima commodi unde ipsa rerum asperiores vel! </p>
+                <button class="post-card-button">Read More</button>
+            </div>
+            <div class="post-card-footer">
+                <span>
+                    <i class="far fa-comments"></i>
+                    <p>NO COMMENT</p>
+                </span>
+                <div class="social">
+                    <span>
+                        <i class="fab fa-dribbble"></i>
+                        <i class="fab fa-facebook-f"></i>
+                        <i class="fab fa-twitter"></i>
+                        <i class="fab fa-pinterest"></i>
+                    </span>
+                </div>
+            </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+            <div class="col-lg-3">
+                <div class="post-card white">
+            
+                    <div class="post-card-body">
+                    <div class="post-card-header">
+                        <span class="wrap">
+                            <i class="fas fa-pen-square fa-3x"></i>
+                        </span>
+                    </div>
+                        <h6>Travelling</h6>
+                        <h3>Travel besT Adventure</h3>
+                        <h6>January 25, 2016</h6>
+                        <p>Lorem ipsum doloirs sit amet, csatetur
+                            adipisicin, sed does eiusmod tehampor 
+                            incididunt uts laboasre et dolhvaliqua.
+                            Ut enim  vem, nostrasuaations ullamco
+                            labiosi ut saaliquip exoi asea comoodo
+                            Disco choice is the have great work.</p>
+                        <button class="post-card-button">Read More</button>
+                    </div>
+                    <div class="post-card-footer">
+                        <span>
+                            <i class="far fa-comments"></i>
+                            <p>NO COMMENT</p>
+                        </span>
+                        <div class="social">
+                            <span>
+                                <i class="fab fa-dribbble"></i>
+                                <i class="fab fa-facebook-f"></i>
+                                <i class="fab fa-twitter"></i>
+                                <i class="fab fa-pinterest"></i>
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="post-card white">
+                <div class="post-card-image"> 
+                    <img class="d-block w-100" src="images/banner.jpg" alt=""> 
+                </div>
+                <div class="post-card-body">
+                    <div class="post-card-header">
+                    <span class="wrap">
+                        <i class="fas fa-image fa-3x"></i>
+                    </span>
+                </div>
+                    <h6>Travelling</h6>
+                    <h3>Travel besT Adventure</h3>
+                    <h6>January 25, 2016</h6>
+                    <button class="post-card-button">Read More</button>
+                </div>
+                <div class="post-card-footer">
+                    <span>
+                        <i class="far fa-comments"></i>
+                        <p>NO COMMENT</p>
+                    </span>
+                    <div class="social">
+                        <span>
+                            <i class="fab fa-dribbble"></i>
+                            <i class="fab fa-facebook-f"></i>
+                            <i class="fab fa-twitter"></i>
+                            <i class="fab fa-pinterest"></i>
+                        </span>
+                    </div>
+                </div>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="post-card white">
+                <div class="post-card-body">
+                <div class="post-card-header">
+                    <span class="wrap">
+                        <i class="fas fa-pen-square fa-3x"></i>
+                    </span>
+                </div>
+                    <h6>Travelling</h6>
+                    <h3>Travel besT Adventure</h3>
+                    <h6>January 25, 2016</h6>
+                    <p>Lorem ipsum doloirs sit amet, csatetur <br>
+                        adipisicin, sed does eiusmod tehampor <br>
+                        choice is the have great work.</p>
+                    <button class="post-card-button">Read More</button>
+                </div>
+                <div class="post-card-footer">
+                    <span>
+                        <i class="far fa-comments"></i>
+                        <p>NO COMMENT</p>
+                    </span>
+                    <div class="social">
+                        <span>
+                            <i class="fab fa-dribbble"></i>
+                            <i class="fab fa-facebook-f"></i>
+                            <i class="fab fa-twitter"></i>
+                            <i class="fab fa-pinterest"></i>
+                        </span>
+                    </div>
+                </div>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="post-card white">
+                <div class="post-card-body">
+                <div class="post-card-header">
+                    <span class="wrap">
+                        <i class="fas fa-pen-square fa-3x"></i>
+                    </span>
+                </div>
+                    <h6>Travelling</h6>
+                    <h3>Travel besT Adventure</h3>
+                    <h6>January 25, 2016</h6>
+                    <p> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum, eos doloremque magni sequi officiis maiores quis aliquid recusandae nihil nobis, error laboriosam beatae iusto suscipit commodi illum eum, iste esse. Facilis placeat nobis minima pariatur iste minus, expedita corporis cupiditate officiis quam unde sapiente! Consectetur, et id voluptatum provident placeat officia illum quas minima commodi unde ipsa rerum asperiores vel! </p>
+                    <button class="post-card-button">Read More</button>
+                </div>
+                <div class="post-card-footer">
+                    <span>
+                        <i class="far fa-comments"></i>
+                        <p>NO COMMENT</p>
+                    </span>
+                    <div class="social">
+                        <span>
+                            <i class="fab fa-dribbble"></i>
+                            <i class="fab fa-facebook-f"></i>
+                            <i class="fab fa-twitter"></i>
+                            <i class="fab fa-pinterest"></i>
+                        </span>
+                    </div>
+                </div>
+                </div>
+            </div>
+    </div>
+</div>
---
+++ templates/slider-template.php
@@ -0,0 +1,46 @@
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-lg-12">                
+            <div id="carousel-slider" class="carousel slide" data-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img class="d-block w-100" src="images/banner.jpg" alt="Первый слайд">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h2>Тупой</h2>
+                            <p>вот так вот сосок дырявый!</p>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100" src="images/banner.jpg" alt="Второй слайд">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h2>Тупой</h2>
+                            <p>вот так вот сосок дырявый!</p>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100" src="images/banner.jpg" alt="Третий слайд">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h2>Тупой</h2>
+                            <p>вот так вот сосок дырявый!</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-control">
+                    <a class="carousel-control-prev" href="#carousel-slider" role="button" data-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <ol class="carousel-indicators">
+                        <li data-target="#carousel-slider" data-slide-to="0" class="active"></li>
+                        <li data-target="#carousel-slider" data-slide-to="1"></li>
+                        <li data-target="#carousel-slider" data-slide-to="2"></li>
+                    </ol>
+                    <a class="carousel-control-next" href="#carousel-slider" role="button" data-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
+                </div>                    
+            </div>
+        </div>
+    </div>
+</div>
