f90db3ca89123573cd81843e2f5c4d41e463523c
AlexandrMonakhov <31701404+AlexandrMonakhov@users.noreply.github.com>
2019-02-07 16:04:22 +0300

    admin_panel

--- admin-panel.php
+++ admin-panel.php
@@ -9,26 +9,29 @@
     <title>Bodybuilding Blog</title>
 </head>
 <body>
-<form class="form-inline">
-  <label class="sr-only" for="inlineFormInputName2">Name</label>
-  <input type="text" class="form-control mb-2 mr-sm-2" id="inlineFormInputName2" placeholder="Jane Doe">
-
-  <label class="sr-only" for="inlineFormInputGroupUsername2">Username</label>
-  <div class="input-group mb-2 mr-sm-2">
-    <div class="input-group-prepend">
-      <div class="input-group-text">@</div>
-    </div>
-    <input type="text" class="form-control" id="inlineFormInputGroupUsername2" placeholder="Username">
-  </div>
-
-  <div class="form-check mb-2 mr-sm-2">
-    <input class="form-check-input" type="checkbox" id="inlineFormCheck">
-    <label class="form-check-label" for="inlineFormCheck">
-      Remember me
-    </label>
-  </div>
-  <button type="submit" class="btn btn-primary mb-2">Submit</button>
-</form>
+<div class="admin-panel">
+    <form class="form-horizontal" role="form">
+        <div class="form-group">
+        <label class="col-md-2 control-label">E-mail</label>
+        <div class="col-md-10">
+        <input type="email" class="form-control" placeholder="Your e-mail">
+        </div>
+        </div>
+        <div class="form-group">
+        <label class="col-md-2 control-label">Password</label>
+        <div class="col-md-10">
+        <input type="password" class="form-control" placeholder="Your Password">
+        </div>
+        </div>
+        <div class="form-group">
+        </div>
+        <div class="form-group">
+        <div class="col-md-2 col-md-offset-10">
+        <button type="submit">Sign In</button>
+        </div>
+        </div>
+    </form>
+</div>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
 <script src="js/bootstrap.min.js"></script>
